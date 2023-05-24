422c82968634ef9df470753a497d45a075207932
riyaalexander <riyaalexander@Riyas-Air.fios-router.home>
2023-05-24 10:40:24 -0400

    edits

--- src/Pages/RecipeDetails.jsx
+++ src/Pages/RecipeDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import '/Users/riyaalexander/Documents/Pursuit/Mod/foodies-unite-fe/src/Styles/RecipeDetails.css';
+import '../Styles/RecipeDetails.css';
 
 
 
